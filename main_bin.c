c5eb9e50fb045c54076c9dbdd907467bb7fbd744
musietsehay <musietsehay@gmail.com>
2022-11-24 23:55:09 -0800

    updatefor Q2

---
+++ main_bin.c
@@ -0,0 +1,22 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int mainx(void)
+
+{
+
+	    _printf("%b\n",98);
+
+	        return (0);
+
+}
